67c90b8c7428e5140edb2a589ea2f5ae7a89241f
Света <silverbrille@gmail.com>
2020-11-03 21:03:42 +0300

    script for catalog

--- src/js/script.js
+++ src/js/script.js
@@ -23,6 +23,23 @@ $(document).ready(function(){
       .closest('div.container').find('div.catalog__content').removeClass('catalog__content_activ')
       .eq($(this).index()).addClass('catalog__content_activ');
   });
+
+  $('.catalog-item__content').addClass('catalog-item__content_activ');
+  $('.catalog-item__list').removeClass('catalog-item__list_activ');
+
+  function toggleCatalog(item){
+    $(item).each(function(i){
+      $(this).on('click', function(e){
+        e.preventDefault();
+        $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_activ');
+        $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_activ');
+
+      })
+    })
+  }
+
+  toggleCatalog('.catalog-item__link');
+  toggleCatalog('.catalog-item__back');
     
   });
 
